d3a8bf3b8b4c79a0760823d1aa6a1568a2b53443
mirac <miracbalkaya029@gmail.com>
2021-10-26 14:46:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay gameplay= new Gameplay();
+
+        obj.setBounds(10,10,905,700);
+        obj.setBackground(Color.DARK_GRAY);;
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+}
